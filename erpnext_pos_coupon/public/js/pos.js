f0172365af476899a8ace789beb0d32aba5e547d
Fadil <Fadil>
2021-02-05 18:41:12 +0000

    coupon button duplication removed

--- erpnext_pos_coupon/public/js/pos.js
+++ erpnext_pos_coupon/public/js/pos.js
@@ -7,6 +7,56 @@ function page_changed(event)
 {
     frappe.after_ajax(function () 
     {
+        // creates a button to toggle item-cart and pos-cart
+
+        var button_order = document.createElement('button');            // creates a button
+        button_order.innerHTML = 'Show/Hide Order';                     // assign name of button
+        button_order.classList.add('show_order');                       // assign a class for button
+        button_order.onclick = function () {                            // onclick of button, item-cart and pos cart toggles
+        $(".pos-cart").fadeToggle("3000");      
+        }
+        var button_bar = document.createElement("DIV");                 // created a div
+        button_bar.appendChild(button_order);                           // button is appended to the div
+        button_bar.classList.add("button-bar");                         // assigned a class for div
+        let pos = $('.pos');
+        if($('.button-bar').length == 0){
+            pos.prepend(button_bar);                
+        }
+
+        // creates a button for pay in mobile pos
+
+        var totalElm = document.createElement('DIV');                   // a div is created
+        totalElm.classList.add('col-8')                                 // class is assigned to the div
+        var button_pay = document.createElement('button');              // created a button
+        button_pay.innerHTML = 'Pay';                                   // assign name of button
+        button_pay.classList.add("col-4");                              // assigned a class for button
+        let pos_pay = $('div.num-col.brand-primary');
+        button_pay.onclick = function () {                              // on click of mobile pos pay, pay button of pos is clicked and route into payments.
+
+            $('div.num-col.brand-primary').click();
+            console.log('paid');   
+
+        }
+        
+        var bottom_toolbar = document.createElement("DIV");
+        bottom_toolbar.appendChild(totalElm)
+        bottom_toolbar.appendChild(button_pay)
+                   
+        bottom_toolbar.classList.add("fixed-bottom");
+        bottom_toolbar.classList.add("row");
+        bottom_toolbar.classList.add("col-12");
+
+        pos.append(bottom_toolbar);
+
+        let itemSelect = document.querySelector('.pos-items-wrapper');
+        
+        let mposAmount = document.querySelector('.col-8');
+        let roundTotal = document.querySelector('.rounded-total-value')
+
+            window.setInterval( function() {
+            let totalAmount = document.querySelector('.grand-total-value');
+            mposAmount.innerText =  totalAmount.textContent;
+            },100);
         var route = frappe.get_route();
         if(route[0] == "pos")
         {
@@ -26,7 +76,7 @@ function page_changed(event)
             var button_off = document.createElement('button');
             button_off.classList.add('coupon_button')
             button_off.style.color = "white";
-            button_off.innerHTML = 'Add Coupons';
+            button_off.innerHTML = 'Add Coupon';
             let container = document.querySelector('div.clearfix');
             container.appendChild(button_off).style.backgroundColor = "#5e64ff";
             button_off.onclick = function()
@@ -34,75 +84,7 @@ function page_changed(event)
                 d.show();
             }
 
-            var button_order = document.createElement('button');
-            button_order.innerHTML = 'Show/Hide Order';
-            button_order.classList.add('show_order');
-     
-            button_order.onclick = function () {
-                
-            $(".pos-cart").fadeToggle("3000");
-            
-                
-            }
 
-            var button_bar = document.createElement("DIV");
-            
-            button_bar.appendChild(button_order);
-            button_bar.classList.add("button-bar");
-
-            let pos = $('.pos');
-            
-            if($('.button-bar').length == 0){
-                pos.prepend(button_bar);                
-            }
-           
-             
-            /*var input = document.createElement('input');
-            input.setAttribute("type", "text");
-            input.setAttribute("value", "Total");
-            console.log("okkkk")
-            input.innerHTML = 'Total Amount';
-            input.classList.add("col-8");*/
-
-            net_total_text = document.querySelector('div.net-total').textContent // extracting net total value
-            net_total_int = net_total_text.match(/\d+/)[0] //extracting integer values from net-total array as string
-            net_total = parseInt(net_total_int) // converting string to integer
-
-            var totalElm = document.createElement('DIV');
-            //totalElm.innerHTML = net_total ;
-            totalElm.classList.add('col-8')
-            
-            var button_pay = document.createElement('button');
-            button_pay.innerHTML = 'Pay';
-
-            button_pay.classList.add("col-4");
-            let pos_pay = $('div.num-col.brand-primary');
-            button_pay.onclick = function () {
-
-                $('div.num-col.brand-primary').click();
-                console.log('paid');   
-
-            }
-            
-            var bottom_toolbar = document.createElement("DIV");
-            bottom_toolbar.appendChild(totalElm)
-            bottom_toolbar.appendChild(button_pay)
-                       
-            bottom_toolbar.classList.add("fixed-bottom");
-            bottom_toolbar.classList.add("row");
-            bottom_toolbar.classList.add("col-12");
-
-            pos.append(bottom_toolbar);
-
-            let itemSelect = document.querySelector('.pos-items-wrapper');
-            
-            let mposAmount = document.querySelector('.col-8');
-            let roundTotal = document.querySelector('.rounded-total-value')
-
-                window.setInterval( function() {
-                let totalAmount = document.querySelector('.grand-total-value');
-                mposAmount.innerText =  totalAmount.textContent;
-                },100);
         }
     })
 }
